a2ce57e90fa2e5642bb7b5115cea8b65fd0c4057
leonore14 <63911892+leonore14@users.noreply.github.com>
2020-06-03 18:15:42 +0200

    fixing modals

--- script.js
+++ script.js
@@ -11,54 +11,18 @@ let closemodal = document.getElementById('close-modal');
   modal.style.display = "none";
   });
 
+
+let closemodalsend = document.getElementById('close-modal-send');
+  closemodalsend.addEventListener('click', () => {
+  modal.style.display = "none";
+  });
+
 // END OF INDEX PAGE MODAL //
   
 
-// COURSES PAGE MODAL 1 //
-
-let openmodal1 = document.getElementById('apply-now-modal-1');
-let modal1 = document.getElementById('modal-1');
-  openmodal1.addEventListener('click', () => {
-  modal1.style.display = "block";
-  });
-
-let closemodal1 = document.getElementById('close-modal-1');
-  closemodal1.addEventListener('click', () => {
-  modal1.style.display = "none";
-  });
-
-// END OF COURSES PAGE MODAL 1 //
-
-
-// COURSES PAGE MODAL 2 //
-
-let openmodal2 = document.getElementById('apply-now-modal-2');
-let modal2 = document.getElementById('modal-2');
-  openmodal2.addEventListener('click', () => {
-  modal2.style.display = "block";
-  });
-
-let closemodal2 = document.getElementById('close-modal-2');
-  closemodal2.addEventListener('click', () => {
-  modal2.style.display = "none";
-  });
-
-// END OF COURSES PAGE MODAL 2 //
 
 
 
-// COURSES PAGE MODAL 3 //
 
-let openmodal3 = document.getElementById('apply-now-modal-3');
-let modal3 = document.getElementById('modal-3');
-  openmodal3.addEventListener('click', () => {
-  modal3.style.display = "block";
-  });
 
-let closemodal3 = document.getElementById('close-modal-3');
-  closemodal3.addEventListener('click', () => {
-  modal3.style.display = "none";
-  });
-
-// END OF COURSES PAGE MODAL 3 //
 
